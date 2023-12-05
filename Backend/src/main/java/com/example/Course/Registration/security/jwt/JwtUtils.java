5f3307e12dc435156712a16eb741496b8648e6e8
Shyam Sundar Mylai <shyam@Shyams-MacBook-Air.local>
2023-12-05 16:09:37 -0500

    secret key change

--- Backend/src/main/java/com/example/Course/Registration/security/jwt/JwtUtils.java
+++ Backend/src/main/java/com/example/Course/Registration/security/jwt/JwtUtils.java
@@ -19,10 +19,10 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-  @Value("${bezkoder.app.jwtSecret}")
+  @Value("${courseReigstration.app.jwtSecret}")
   private String jwtSecret;
 
-  @Value("${bezkoder.app.jwtExpirationMs}")
+  @Value("${courseReigstration.app.jwtExpirationMs}")
   private int jwtExpirationMs;
 
   public String generateJwtToken(Authentication authentication) {
