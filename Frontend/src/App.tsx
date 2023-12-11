15a71047a8e84af470a9ea28408f7dab28cc7fc8
Anvitha Lakshmisha <98431766+anvithalakshmisha@users.noreply.github.com>
2023-12-11 12:06:15 -0500

    add shadcn ui table | update some column defs

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,15 @@
 
 import './App.css'
-import { Button } from '@/components/ui/button.tsx'
+import { Button } from './components/ui/button'
+import { columns } from './components/ui/Table/Columns'
+import { DataTable } from './components/ui/Table/dataTable'
 
 function App() {
 
   return (
     <>
       <Button>Course Registartion</Button>
+      <DataTable columns={columns} data={[]}></DataTable>
     </>
   )
 }
