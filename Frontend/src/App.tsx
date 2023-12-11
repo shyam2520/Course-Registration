a68ffda9971d1d53cc78e3f2d34b45555e088769
Anvitha Lakshmisha <98431766+anvithalakshmisha@users.noreply.github.com>
2023-12-11 12:16:26 -0500

    merge

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,18 +3,24 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import './App.css';
 import AuthProvider from './providers/AuthProvider';
 import { Route } from './providers/Route';
-
+import { Button } from './components/ui/button'
+import { columns } from './components/ui/Table/Columns'
+import { DataTable } from './components/ui/Table/dataTable'
 
 function App() {
 
   const queryClient = new QueryClient();
 
   return (
+    <>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Route />
       </AuthProvider>
     </QueryClientProvider>
+      <Button>Course Registartion</Button>
+      <DataTable columns={columns} data={[]}></DataTable>
+    </>
   )
 }
 
